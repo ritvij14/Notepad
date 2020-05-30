be31627200d14bb9ae07d039de419d85b6589e22
Ritvij Kumar Sharma <ritvij2001@gmail.com>
2020-05-30 23:36:39 +0530

    Creating project

---
+++ app/src/main/java/com/example/notepad/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.notepad.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.notepad.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
